530e2734a4e180be77d45fd85f4c4c66aa74da68
Simon Shephard <simonshephard@hotmail.com>
2018-08-24 07:34:21 +0100

    adds show transaction and partial new transaction

--- controller.rb
+++ controller.rb
@@ -6,12 +6,31 @@ also_reload("./models/*")
 
 
 # HOME
-get '/' do
+get "/" do
   erb(:home)
 end
 
 # INDEX
-get '/transactions' do
+get "/transactions" do
   @transactions = Transaction.all
   erb(:index)
+end
+
+# NEW - above show to avoid conflict
+get '/transactions/new' do
+  erb(:new)
+end
+
+# SHOW
+get '/transactions/:id' do
+  @transaction = Transaction.find(params[:id])
+  erb(:show)
+end
+# what are standard REST for show / create
+
+# CREATE
+post '/transactions' do
+  transaction = Transaction.new(params)
+  transaction.save
+  redirect to '/transactions'
 end
