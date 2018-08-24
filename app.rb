410655fd08759dd0d7582e53b0473b546f6c6ada
Simon Shephard <simonshephard@hotmail.com>
2018-08-24 15:11:38 +0100

    adds tags controller

--- app.rb
+++ app.rb
@@ -2,6 +2,7 @@ require("sinatra")
 require("sinatra/contrib/all")
 require_relative("controllers/transactions_controller")
 require_relative("controllers/merchants_controller")
+require_relative("controllers/tags_controller")
 
 
 get "/" do
---
+++ controllers/tags_controller.rb
@@ -0,0 +1,52 @@
+require("sinatra")
+require("sinatra/contrib/all")
+require("pry-byebug")
+require_relative("../models/transaction")
+require_relative("../models/tag")
+require_relative("../models/merchant")
+also_reload("../models/*")
+
+
+# INDEX
+get "/tags" do
+  @tags = Tag.all
+  erb(:"tags/index")
+end
+
+# NEW
+get '/tags/new' do
+  erb(:"tags/new")
+end
+
+# SHOW
+get '/tags/:id' do
+  @tag = Tag.find(params[:id])
+  erb(:"tags/show")
+end
+
+# CREATE
+post '/tags' do
+  tag = Tag.new(params)
+  tag.save
+  redirect to '/tags'
+end
+
+# EDIT
+get '/tags/:id/edit' do
+  @tag = Tag.find(params[:id])
+  erb(:"tags/edit")
+end
+
+# UPDATE
+post '/tags/:id' do
+  tag = Tag.new(params)
+  tag.update
+  redirect to "/tags/#{tag.id}"
+end
+
+# DELETE
+post '/tags/:id/delete' do
+  tag = Tag.find(params[:id])
+  tag.delete
+  redirect to '/tags'
+end
