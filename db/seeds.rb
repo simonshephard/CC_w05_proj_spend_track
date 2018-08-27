96dca5d4651f0402c4fe916b55bf9b151b3afad5
Simon Shephard <simonshephard@hotmail.com>
2018-08-27 17:08:29 +0100

    adds budget table and seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,28 +1,17 @@
-require_relative("../models/merchant.rb")
+require_relative("../models/budget.rb")
 require_relative("../models/tag.rb")
+require_relative("../models/merchant.rb")
 require_relative("../models/transaction.rb")
 require("pry-byebug")
 
 
 Transaction.delete_all
-Tag.delete_all
 Merchant.delete_all
+Tag.delete_all
+Budget.delete_all
 
 
-merchant0 = Merchant.new({"name" => "OTHER"})
-merchant1 = Merchant.new({"name" => "Asda"})
-merchant2 = Merchant.new({"name" => "Sainsburys"})
-merchant3 = Merchant.new({"name" => "Tesco"})
-merchant4 = Merchant.new({"name" => "Aldi"})
-merchant5 = Merchant.new({"name" => "Lothian Buses"})
-merchant6 = Merchant.new({"name" => "IKEA"})
-merchant0.save
-merchant1.save
-merchant2.save
-merchant3.save
-merchant4.save
-merchant5.save
-merchant6.save
+budget1 = Budget.new({"amount" => "1000.00"})
 
 tag0 = Tag.new({"name" => "OTHER"})
 tag1 = Tag.new({"name" => "Food"})
@@ -38,6 +27,21 @@ tag3.save
 tag4.save
 tag5.save
 tag6.save
+
+merchant0 = Merchant.new({"name" => "OTHER"})
+merchant1 = Merchant.new({"name" => "Asda"})
+merchant2 = Merchant.new({"name" => "Sainsburys"})
+merchant3 = Merchant.new({"name" => "Tesco"})
+merchant4 = Merchant.new({"name" => "Aldi"})
+merchant5 = Merchant.new({"name" => "Lothian Buses"})
+merchant6 = Merchant.new({"name" => "IKEA"})
+merchant0.save
+merchant1.save
+merchant2.save
+merchant3.save
+merchant4.save
+merchant5.save
+merchant6.save
 
 transaction1 = Transaction.new({"date_time" => "2018-06-01T10:30", "amount" => "10.50", "description" => "Food", "merchant_id" => merchant1.id, "tag_id" => tag1.id})
 transaction2 = Transaction.new({"date_time" => "2018-07-01T11:30", "amount" => "12.99", "description" => "Food", "merchant_id" => merchant2.id, "tag_id" => tag1.id})
